0f0025afe778401acf30bf45fd6ee2aadd665644
Lescenco Andrei Bogdan <andrei.lescenco@sparktech.ro>
2012-02-10 19:00:07 +0200

    added placeholder instead of value

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -48,7 +48,8 @@ class UsersController < ApplicationController
     @user.groups << group
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        session[:user_id] = @user.id
+        format.html { redirect_to login_index_path, notice: 'User was successfully created.' }
         format.json { render json: @user, status: :created, location: @user }
       else
         format.html { render action: "new" }
