9b01e372918d28c3c5dec240bda8cdfbfd21e193
Andrei Lescenco <lescee@email.com>
2012-02-13 05:51:26 +0200

    zzzzz

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,6 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
-    #if current_user.role? :admin
     @users = User.all
     respond_to do |format|
       format.html # index.html.erb
@@ -65,7 +64,7 @@ class UsersController < ApplicationController
 
     respond_to do |format|
       if @user.update_attributes(params[:user])
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to '/', notice: 'User was successfully updated.' }
         format.json { head :ok }
       else
         format.html { render action: "edit" }
